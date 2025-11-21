85e158c49195c6662fc5f21795644c3e36f21217
Morgan <h24morgr@du.se>
2025-11-21 12:49:53 +0100

    Klar

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+
+// UPPGIFT 2 ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
 {
     let Hello = 1;
     var World = 1;
@@ -10,8 +13,10 @@ console.log(typeof foo);
 
 /*
     Alla loggar "undefined" såvida de inte är tilldelade ett värde. Dock om har ett värde så fungerar de som förväntat, let kan
-    bara nås inom blocket, samma med const. Var fungerar dock i global scope, dvs. vart som helst i dokumentet.
+    bara nås inom blocket, samma med const. Var kan dock nås i global scope, dvs. vart som helst i dokumentet.
 */
+
+// UPPGIFT 3 ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 console.log('5' == 5);               // True, värde jämförelse
 console.log('5' === 5);              // False, värde och typ jämförelse
@@ -23,8 +28,8 @@ console.log(NaN == 5);               // False
 console.log(NaN === 5);              // False
 console.log(NaN == '5');             // False
 console.log(NaN === '5');            // False
-console.log(NaN != '5')              // True "Inte ett nummer är inte lika med ett nummer" --> Sant.
-console.log(NaN !== '5')             // True "Inte ett nummer är inte lika med ett nummer" --> Sant.
+console.log(NaN != '5')              // True "NaN är inte lika med ett nummer"  --> Sant.
+console.log(NaN !== NaN)             // True "NaN är inte lika med NaN"         --> Sant.
 console.log("Slutet på NaN bool-checkar")
 
 
@@ -41,11 +46,14 @@ console.log(undefined ? true : false)   // False, undefined är inte true.
 
     När ett uttryck står för sig självt blir det en bool-check och uttrycket kommer returnera antingen true eller false.
 
-    NaN står för "Not a number" och returnerar false för alla typer av booliska jämförelser, med ett undantag som demonstreras ovan.
+    NaN står för "Not a number" och returnerar false för alla typer av booliska jämförelser då man jämför "lika med". Då man jämför "inte lika med" får man alltid ut true.
+    Då NaN inte är "lika med" någonting är det också alltid "inte lika med" allting annat.
     undefined står för "odefinierad" och menar på att det saknas ett värde.
     null står för "inget" och är ett tomt värde.
-    Skillnaden mellan undefined och null är att null kan användas med avsikt och undefined är oftast returnerat då man gjort ett misstag och inte använt variabelvärdestilldelning korrekt.
+    Skillnaden mellan undefined och null är att null menar på att något har ett "tomt" värde medans i undefined har man inte specifierat ett värde.
 */
+
+// UPPGIFT 4 ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 //console.log(greet(name)) // Kastar ett ReferenceError  error-meddelande.
 
@@ -77,7 +85,7 @@ console.log(greet(name)) // Returnerar "Hej på mig"
     Och kan även skapas på en rad:
     const greet = name => "Hej " + name;
     
-    Dessa funktioner skapas dynamiskt då de exekveras i programmet.
+    Dessa funktioner skapas när de körs i programmet. (Dvs. de skapas inte vid anropnig/exekvering utan när programraden har nåtts utav programmet.)
 */
 
 /*
@@ -89,5 +97,17 @@ console.log(greet(name)) // Returnerar "Hej på mig"
     Funktionsuttryck är nära lika funktionsdeklaration och är också tydliga samt enkla att förstå sig på.
     Det är ett värde som innehåller en funktion.
     Den är inte tillgänglig förens exekvering, dvs. programmet måste nå den i körtid innan den kan användas.
+*/
 
+/*
+    Om jag ändrar på parametern name i den övre greet-funktions-argumentet så uppstår ett ReferenceError.   "ame is not defined"
+    Om jag ändrar på parametern name i retur-satsen så uppstår ett ReferenceError.                          "Cannot access 'name' before initialization"
+    Om jag ändrar på parametern name i den nedre greet-funktions-argumentet uppstår ett ReferenceError.     "ame is not defined"
+
+*/
+
+/*
+    Parameter är en typ av variabel som refererar till en typ av inputdata som en funktion använder sig utav. Man kan tänka sig att de fungerar som en placeholder som tar emot riktig data och pekar mot vart datan ska.
+    Variabeler är namngiven data som lagras i minnet. En variabel kan ta emot en mångfald olika sorters data. Till skillnad från parametrar så lagras datan alltid i en standard variabel, dessutom kan de existera utanför local scope.
+    Argument är det faktiska värdet (t.ex mha. en variabel) du skickar till en parameter vilket sedan kan användas inuti en funktion.
 */
